aa5f43b6a4dd9b7158fd824848877edecf8fa94e
lianchonghui <lianchonghui@foxmail.com>
2018-01-28 13:08:38 +0800

    fix settings

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from Django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView
 
 from blog.feeds import AllPostsRssFeed
 from blog.sitemaps import sitemaps
